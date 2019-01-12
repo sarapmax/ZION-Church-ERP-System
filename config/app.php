6439e76b0c7e816a67ee6c847fa1413d27291c75
Teerpong Phothiphun <teerpong.me@gmail.com>
2019-01-12 13:54:31 +0800

    Add Laravel Enum library

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Bangkok',
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'th',
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Nasyrov\Laravel\Enums\EnumServiceProvider::class,
 
     ],
 
