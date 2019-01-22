9e91aefcb23718d3b8fd3ca5160d95e33716ce09
Teerpong Phothiphun <teerpong.me@gmail.com>
2019-01-22 22:23:39 +0800

    Disable factories when it's on production.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,30 +18,32 @@ class DatabaseSeeder extends Seeder
         $this->call(SubDistrictsTableSeeder::class);
         $this->call(PostcodesTableSeeder::class);
 
-        // Wipe up 5 churches, and each church has 5 cells, and each cell has 20 members associated with.
-        // In other word, we wipe up 500 church members.
-        for ($i = 0; $i < 5; $i++) {
-            $church = factory(\App\Models\Church::class)->create();
+        if (env('APP_ENV') !== 'production') {
+            // Wipe up 5 churches, and each church has 5 cells, and each cell has 20 members associated with.
+            // In other word, we wipe up 500 church members.
+            for ($i = 0; $i < 5; $i++) {
+                $church = factory(\App\Models\Church::class)->create();
 
-            $church->cells()->saveMany(factory(\App\Models\Cell::class, 5)->create()->each(function($cell) {
-                $cell->members()->saveMany(factory(App\Models\Member::class, 20)->create()->each(function($member) {
-                    $member->addresses()->save(factory(\App\Models\Address::class)->make([
-                        'type' => \App\Enums\AddressTypeEnum::CURRENT
-                    ]));
+                $church->cells()->saveMany(factory(\App\Models\Cell::class, 5)->create()->each(function($cell) {
+                    $cell->members()->saveMany(factory(App\Models\Member::class, 20)->create()->each(function($member) {
+                        $member->addresses()->save(factory(\App\Models\Address::class)->make([
+                            'type' => \App\Enums\AddressTypeEnum::CURRENT
+                        ]));
 
-                    $member->addresses()->save(factory(App\Models\Address::class)->make([
-                        'type' => \App\Enums\AddressTypeEnum::ORIGINAL
-                    ]));
+                        $member->addresses()->save(factory(App\Models\Address::class)->make([
+                            'type' => \App\Enums\AddressTypeEnum::ORIGINAL
+                        ]));
 
-                    $member->mariage()->save(factory(\App\Models\Mariage::class)->make());
+                        $member->mariage()->save(factory(\App\Models\Mariage::class)->make());
 
-                    $emergencyContact = $member->emergencyContact()->save(factory(\App\Models\EmergencyContact::class)->make());
+                        $emergencyContact = $member->emergencyContact()->save(factory(\App\Models\EmergencyContact::class)->make());
 
-                    $emergencyContact->address()->save(factory(\App\Models\Address::class)->make([
-                        'type' => \App\Enums\AddressTypeEnum::EMERGENCY
-                    ]));
+                        $emergencyContact->address()->save(factory(\App\Models\Address::class)->make([
+                            'type' => \App\Enums\AddressTypeEnum::EMERGENCY
+                        ]));
+                    }));
                 }));
-            }));
+            }
         }
     }
 }
