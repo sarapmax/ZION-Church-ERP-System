a0166fd5fbcde167d30bee26467d03883d272434
Teerpong Phothiphun <teerpong.me@gmail.com>
2019-02-01 23:52:41 +0800

    Update partials style.

--- resources/views/components/actions/delete.blade.php
+++ resources/views/components/actions/delete.blade.php
@@ -1,5 +1,5 @@
 <a href="javascript:void(0);" class="{{ $type == 'button' ? 'btn btn-white' : '' }}" onclick="$(this).find('form').submit();" >
-    <i class="far fa-trash-alt"></i>
+    <i class="fe fe-trash"></i>
     <form action="{{ $route }}" method="POST" onsubmit="return confirm('Are you sure to delete this?')">
         @csrf
         {{ method_field('DELETE') }}
--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,29 +1,31 @@
 @if ($paginator->hasPages())
     <hr/>
-    <div class="float-sm-left">
-        Showing {{ $paginator->firstItem() }} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} entries
-    </div>
-    <ul class="pagination float-sm-right mt-2 mt-sm-0" role="navigation">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                <span class="page-link" aria-hidden="true">@lang('pagination.previous')</span>
-            </li>
-        @else
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">@lang('pagination.previous')</a>
-            </li>
-        @endif
+    <div class="container-fluid">
+        <div class="float-sm-left">
+            Showing {{ $paginator->firstItem() }} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} entries
+        </div>
+        <ul class="pagination float-sm-right mt-2 mt-sm-0" role="navigation">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">@lang('pagination.previous')</span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">@lang('pagination.previous')</a>
+                </li>
+            @endif
 
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">@lang('pagination.next')</a>
-            </li>
-        @else
-            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                <span class="page-link" aria-hidden="true">@lang('pagination.next')</span>
-            </li>
-        @endif
-    </ul>
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">@lang('pagination.next')</a>
+                </li>
+            @else
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link" aria-hidden="true">@lang('pagination.next')</span>
+                </li>
+            @endif
+        </ul>
+    </div>
 @endif
