ebdeda21df67af2d050cee6ace4a3d32976a2016
Teerpong Phothiphun <teerpong.me@gmail.com>
2019-02-02 10:47:47 +0800

    Update cell CRUD style.

--- resources/views/membership/cell/create.blade.php
+++ resources/views/membership/cell/create.blade.php
@@ -5,44 +5,46 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="element-wrapper">
-                <h5 class="element-header">
-                    เพิ่มกลุ่มแคร์
-                </h5>
-                <div class="element-box">
-                    <form action="{{ route('membership.cell.store') }}" method="POST">
-                        @csrf
+<div class="header mt-3">
+    <div class="header-body">
+        <h1 class="header-title">เพิ่มกลุ่มแคร์</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('membership.cell.store') }}" method="POST">
+                    @csrf
 
-                        @include('components.selections.church-structure', [
-                            'old' => [
-                                'province_id' => old('province_id'),
-                                'district_id' => old('district_id'),
-                                'church_id' => old('church_id'),
-                            ],
-                            'excepts' => ['cell']
-                        ])
+                    @include('components.selections.church-structure', [
+                        'old' => [
+                            'province_id' => old('province_id'),
+                            'district_id' => old('district_id'),
+                            'church_id' => old('church_id'),
+                        ],
+                        'excepts' => ['cell']
+                    ])
 
-                        <div class="form-group">
-                            <label for="name">ชื่อกลุ่มแคร์ </label>
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                   name="name" value="{{ old('name') }}">
+                    <div class="form-group">
+                        <label for="name">ชื่อกลุ่มแคร์ </label>
+                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                               name="name" value="{{ old('name') }}">
 
-                            @if ($errors->has('name'))
-                                <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                            @endif
-                        </div>
+                        @if ($errors->has('name'))
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                        @endif
+                    </div>
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary" name="submit_type" value="{{ \App\Enums\SubmissionTypeEnum::ADD }}"><i class="fas fa-check"></i> บันทึก</button>
-                            <button type="submit" class="btn btn-primary" name="submit_type" value="{{ \App\Enums\SubmissionTypeEnum::ADD_AND_ADD_ANOTHER }}"><i class="fas fa-check-double"></i> บันทึก & บันทึกอันต่อไป</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" name="submit_type" value="{{ \App\Enums\SubmissionTypeEnum::ADD }}"><i class="fas fa-check"></i> บันทึก</button>
+                        <button type="submit" class="btn btn-primary" name="submit_type" value="{{ \App\Enums\SubmissionTypeEnum::ADD_AND_ADD_ANOTHER }}"><i class="fas fa-check-double"></i> บันทึก & บันทึกอันต่อไป</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/membership/cell/index.blade.php
+++ resources/views/membership/cell/index.blade.php
@@ -5,67 +5,59 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="element-wrapper">
-                <div class="element-actions">
-                    <a href="{{ route('membership.cell.create') }}" class="btn btn-primary">เพิ่มกลุ่มแคร์</a>
-                </div>
-                <h5 class="element-header">
-                    กลุ่มแคร์
-                </h5>
-                <div class="element-box">
-                    <div class="controls-above-table">
-                        <div class="row">
-                            <div class="col-sm-6">
-
-                            </div>
-                            <div class="col-sm-6">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="table-responsive">
-                        <table class="table table-lightborder">
-                            <thead>
-                            <tr>
-                                <th>รหัส</th>
-                                <th>จังหวัด</th>
-                                <th>อำเภอ</th>
-                                <th>คริสตจักร</th>
-                                <th class="text-primary">กลุ่มแคร์</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @forelse($cells as $cell)
-                                <tr>
-                                    <td>{{ $cell->id }}</td>
-                                    <td>{{ $cell->church->district->province->name }}</td>
-                                    <td>{{ $cell->church->district->name }}</td>
-                                    <td>{{ $cell->church->name }}</td>
-                                    <td class="text-primary font-weight-bold">{{ $cell->name }}</td>
-                                    <td class="link-action">
-                                        <a href="{{ route('membership.cell.edit', $cell) }}"><i class="far fa-edit"></i></a>
-
-                                        @include('components.actions.delete', [
-                                            'type' => 'link',
-                                            'route' => route('membership.cell.destroy', $cell)
-                                        ])
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="7" class="text-center">No matching records found!.</td>
-                                </tr>
-                            @endforelse
-                            </tbody>
-                        </table>
-
-                        {{ $cells->appends($_GET)->links() }}
-                    </div>
-                </div>
+<div class="header">
+    <div class="header-body">
+        <div class="row align-items-end">
+            <div class="col"><h1 class="header-title">กลุ่มแคร์</h1></div>
+            <div class="col-auto">
+                <a href="{{ route('membership.cell.create') }}" class="btn btn-primary">เพิ่มกลุ่มแคร์</a>
             </div>
         </div>
     </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="table-responsive mb-0">
+                <table class="table table-nowrap card-table">
+                    <thead>
+                    <tr>
+                        <th>รหัส</th>
+                        <th>จังหวัด</th>
+                        <th>อำเภอ</th>
+                        <th>คริสตจักร</th>
+                        <th class="text-primary">กลุ่มแคร์</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @forelse($cells as $cell)
+                        <tr>
+                            <td>{{ $cell->id }}</td>
+                            <td>{{ $cell->church->district->province->name }}</td>
+                            <td>{{ $cell->church->district->name }}</td>
+                            <td>{{ $cell->church->name }}</td>
+                            <td class="text-primary font-weight-bold">{{ $cell->name }}</td>
+                            <td class="link-action">
+                                <a href="{{ route('membership.cell.edit', $cell) }}"><i class="fe fe-edit"></i></a>
+
+                                @include('components.actions.delete', [
+                                    'type' => 'link',
+                                    'route' => route('membership.cell.destroy', $cell)
+                                ])
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="7" class="text-center">No matching records found!.</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
+                </table>
+
+                {{ $cells->appends($_GET)->links() }}
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
