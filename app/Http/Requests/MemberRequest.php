98274c2c92ba7236bad676014bcc656145ad6552
Teerpong Phothiphun <teerpong.me@gmail.com>
2019-03-07 01:01:12 +0800

    Member email can be left as empty.

--- app/Http/Requests/MemberRequest.php
+++ app/Http/Requests/MemberRequest.php
@@ -69,7 +69,7 @@ class MemberRequest extends FormRequest
             case 'POST':
                 $rules += [
                     'idcard' => ['required', 'alpha_num', 'digits:13', 'unique:users'],
-                    'email' =>  ['required', 'email', 'unique:users']
+                    'email' =>  ['email', 'unique:users']
                 ];
                 break;
 
@@ -78,7 +78,7 @@ class MemberRequest extends FormRequest
             case 'PUT':
                 $rules += [
                     'idcard' => ['required', 'alpha_num', 'digits:13', Rule::unique('users')->ignore($this->route('member'))],
-                    'email' =>  ['required', 'email', Rule::unique('users')->ignore($this->route('member'))]
+                    'email' =>  ['email', Rule::unique('users')->ignore($this->route('member'))]
                 ];
                 break;
         }
